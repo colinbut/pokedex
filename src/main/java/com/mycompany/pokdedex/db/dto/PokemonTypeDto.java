fba76984b2286d68e3cdebe5f08f46ffb0f82da7
Colin But <colinbut@users.noreply.github.com>
2016-09-14 22:40:32 +0100

    implementation for this DTO PokemonTypeDto

--- src/main/java/com/mycompany/pokdedex/db/dto/PokemonTypeDto.java
+++ src/main/java/com/mycompany/pokdedex/db/dto/PokemonTypeDto.java
@@ -6,4 +6,23 @@
 package com.mycompany.pokdedex.db.dto;
 
 public class PokemonTypeDto {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
