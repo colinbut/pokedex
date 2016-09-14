c0271a0557868308ff58d0ab81d5acb2de714920
Colin But <colinbut@users.noreply.github.com>
2016-09-14 23:07:22 +0100

    JDBI mapper for PokemonTypeDto

--- src/main/java/com/mycompany/pokdedex/db/TypeDao.java
+++ src/main/java/com/mycompany/pokdedex/db/TypeDao.java
@@ -6,12 +6,15 @@
 package com.mycompany.pokdedex.db;
 
 import com.mycompany.pokdedex.db.dto.PokemonTypeDto;
+import com.mycompany.pokdedex.db.mapper.JDBIPokemonTypeDtoMapper;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
 
 import java.util.List;
 
 public interface TypeDao {
 
     @SqlQuery("SELECT * FROM POKEMON_TYPE")
+    @Mapper(JDBIPokemonTypeDtoMapper.class)
     List<PokemonTypeDto> fetch();
 }
