8772a3cc220b8c13b574ab0ca94854d830c6f090
Colin But <colinbut@users.noreply.github.com>
2016-09-14 22:44:46 +0100

    finished off PokemonDao for now

--- src/main/java/com/mycompany/pokdedex/db/PokemonDao.java
+++ src/main/java/com/mycompany/pokdedex/db/PokemonDao.java
@@ -5,18 +5,36 @@
  */
 package com.mycompany.pokdedex.db;
 
+import com.mycompany.pokdedex.db.dto.PokemonDto;
+import com.mycompany.pokdedex.db.mapper.PokemonMapper;
 import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
 
+//@RegisterMapper(PokemonMapper.class)
 public interface PokemonDao {
 
     @Deprecated
-    @SqlUpdate("INSERT INTO POKEMON VALUES (:name, :hit_points, :combat_power, :type_id, :pokemon_attack_id)")
+    @SqlUpdate("INSERT INTO POKEMON (name, hit_points, combat_power, type_id) " +
+        "VALUES (:name, :hit_points, :combat_power, :type_id)")
     void insert(@Bind("name") String name,
                 @Bind("hit_points") int hitPoints,
                 @Bind("combat_power") int combatPower,
-                @Bind("type_id") int typeId,
-                @Bind("pokemon_attack_id") int pokemonAttackId);
+                @Bind("type_id") int typeId);
 
+
+    @SqlQuery("SELECT * FROM POKEMON WHERE ID = :id")
+    @Mapper(PokemonMapper.class)
+    PokemonDto fetch(@Bind("id") int id);
+
+    @SqlUpdate("UPDATE POKEMON SET name = :name, hit_points = :hit_points, combat_power = :combat_power, type_id = :type_id")
+    void update(@Bind("name") String name,
+                @Bind("hit_points") int hitPoints,
+                @Bind("combat_power") int combatPower,
+                @Bind("type_id") int typeId);
+
+    @SqlUpdate("DELETE FROM POKEMON WHERE ID = :id")
+    void delete(@Bind("id") int id);
 
 }
