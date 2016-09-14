ca68e3e643cd68ba08811e0918bda5854be30be3
Colin But <colinbut@users.noreply.github.com>
2016-09-14 21:01:06 +0100

    added dropwizard-jdbi dependency for jdbi db support

---
+++ src/main/java/com/mycompany/pokdedex/db/PokemonDao.java
@@ -0,0 +1,9 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.pokdedex.db;
+
+public interface PokemonDao {
+}
