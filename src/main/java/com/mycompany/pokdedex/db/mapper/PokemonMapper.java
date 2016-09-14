3776e108ca3ab7f00b6fa4e2045bc49c313f2187
Colin But <colinbut@users.noreply.github.com>
2016-09-14 22:40:14 +0100

    implementation for this custom JDBI mapper PokemonMapper

--- src/main/java/com/mycompany/pokdedex/db/mapper/PokemonMapper.java
+++ src/main/java/com/mycompany/pokdedex/db/mapper/PokemonMapper.java
@@ -5,5 +5,20 @@
  */
 package com.mycompany.pokdedex.db.mapper;
 
-public class PokemonMapper {
+import com.mycompany.pokdedex.db.dto.PokemonDto;
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PokemonMapper implements ResultSetMapper<PokemonDto> {
+
+    @Override
+    public PokemonDto map(int i, ResultSet resultSet, StatementContext statementContext) throws SQLException {
+        PokemonDto pokemonDto = new PokemonDto();
+        pokemonDto.setId(resultSet.getInt("id"));
+        return pokemonDto;
+    }
+
 }
