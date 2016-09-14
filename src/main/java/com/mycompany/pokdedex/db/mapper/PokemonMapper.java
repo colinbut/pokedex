e7ee0ed67842348f7232900062368642e1c97aa2
Colin But <colinbut@users.noreply.github.com>
2016-09-14 22:35:18 +0100

    PokemonTypeDto and PokemonMapper for JDBI mapping

---
+++ src/main/java/com/mycompany/pokdedex/db/mapper/PokemonMapper.java
@@ -0,0 +1,9 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.pokdedex.db.mapper;
+
+public class PokemonMapper {
+}
